e7c450215827d4190810f42ea122bfa8356bacf8
hazuruir <https://github.com/hazu-4379/hazuruir.git>
2020-04-23 14:55:23 +0900

    java

---
+++ js/js/jspractice.js
@@ -0,0 +1,44 @@
+// document.write("Hello world");
+// alert("hello world");
+
+// console.log("hello world" + "田中さん");
+// console.log(10 + 10);
+// console.log("10" + "10");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// let x= 1;
+// let y=1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// let x= 7;
+// let y= x++;
+
+// console.log(x++);
+// console.log(++x);
+
+
+let age =30;
+const name ="田中太郎";
+
+age = age + 2;
+
+console.log(`${name}さんの年齢は${age}です`);
+
+
+
+
+
+
+
+
+
+
+
