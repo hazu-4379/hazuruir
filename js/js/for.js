85bf4e782eeca8505816a55ea0095b75125845c0
hazuruir <https://github.com/hazu-4379/hazuruir.git>
2020-04-23 16:03:13 +0900

    ren

---
+++ js/js/for.js
@@ -0,0 +1,6 @@
+'use strict';
+const namelist = ["田中","山田","林","佐藤"];
+
+for (let i= 0; i < namelist.lengh ; i++) {
+    console.log(nameList[i]);
+}
--- js/js/js-for.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const namelist = ["田中","山田","林"];
-
-for (let i = 0; i < 2namelist.length ; i++) {
-  console.long(namelist[i]);
-}
